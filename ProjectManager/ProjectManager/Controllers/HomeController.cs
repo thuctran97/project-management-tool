bbdfd091fa30dc262483eef12cbdc0940080ad1b
thuc <thucctin@gmail.com>
2018-03-01 00:10:08 +0700

    Them project manager

---
+++ ProjectManager/ProjectManager/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ProjectManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (QLDAEntities en = new QLDAEntities())
+                {
+                    var v = en.UserLogins.Where(a => a.Account.Equals(u.Account) && a.Password.Equals(u.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserAccount"] = v.Account.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+        public ActionResult AfterLogin()
+        {
+            return View();
+        }
+
+    }
+}
